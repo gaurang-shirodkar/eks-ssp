b5eeb3c7a25c43c1b73972d523f247ce8f92a833
gaurang-shirodkar <gaurang.shirodkar@sysdig.com>
2022-09-11 17:11:19 -0400

    new eks files

---
+++ sysdig-eks/bin/sysdig-eks.ts
@@ -0,0 +1,26 @@
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import * as blueprints from '@aws-quickstart/eks-blueprints';
+
+const app = new cdk.App();
+const account = '712329029476';
+const region = 'us-east-1';
+
+const addOns: Array<blueprints.ClusterAddOn> = [
+    // new blueprints.addons.ArgoCDAddOn,
+    // new blueprints.addons.CalicoAddOn,
+    // new blueprints.addons.MetricsServerAddOn,
+    // new blueprints.addons.ClusterAutoScalerAddOn,
+    // new blueprints.addons.ContainerInsightsAddOn,
+    // new blueprints.addons.AwsLoadBalancerControllerAddOn(),
+    new blueprints.addons.VpcCniAddOn(),
+    new blueprints.addons.CoreDnsAddOn(),
+    new blueprints.addons.KubeProxyAddOn(),
+    // new blueprints.addons.XrayAddOn()
+];
+
+blueprints.EksBlueprint.builder()
+    .account(account)
+    .region(region)
+    .addOns(...addOns)
+    .build(app, 'eks-blueprint');
